aaf01d7e70ff289da1f341853efd899557403a82
dmelloncelli <d.melloncelli@nsi.it>
2015-11-16 15:46:26 +0100

    prova

---
+++ app/scripts/controllers/login.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsCourseApp.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the angularjsCourseApp
+ */
+angular.module('angularjsCourseApp')
+  .controller('LoginCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
