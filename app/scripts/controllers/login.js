136108d27e694a6e66f1218368dadefaed2a4724
dgiacobbe <d.giacobbe@nsi.it>
2015-11-16 16:36:37 +0100

    login

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,14 +8,18 @@
  * Controller of the angularjsCourseApp
  */
 angular.module('angularjsCourseApp')
-  .controller('LoginCtrl', function ()
-  {
+  .controller('LoginCtrl', function ($http) {
     this.credentials = {
-      username : "admin" ,
-      password : "Password1!"}
+      username: "admin",
+      password: "Password1!"
+    }
 
-    this.doLogin = function()
-    {
-
+    this.doLogin = function () {
+      var url = 'https://nsi-prenota.azurewebsites.net';
+      $http.post(url + '/api/Account/Login', {
+        username: vm.credentials.username,
+        password: vm.credentials.password,
+        IsPersistent: true
+      })
     }
   });
