7e991fe91f43a80d81ec37bf92ed83c221c6c47a
dmelloncelli <d.melloncelli@nsi.it>
2015-11-16 13:18:44 +0100

    initial

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularjsCourseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularjsCourseApp
+ */
+angular.module('angularjsCourseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
